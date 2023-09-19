a55061c308d347c5bb92537dfe4b65644d42fbcc
Krishan Mistry <kvjmistry@gmail.com>
2023-09-18 20:41:28 -0500

    Merge in code from ilker

--- source/materials/OpticalMaterialProperties.h
+++ source/materials/OpticalMaterialProperties.h
@@ -87,6 +87,8 @@ namespace opticalprops {
 
   G4MaterialPropertiesTable* STEEL();
 
+  G4MaterialPropertiesTable * PerfectDetector();
+
 
   constexpr G4double optPhotMinE_ =  0.2  * eV;
   constexpr G4double optPhotMaxE_ = 11.5  * eV;
